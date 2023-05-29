fd25bca875574c57ccc6d7af5ab85b4742943bf0
wbhaao <a31620504@gmail.com>
2023-05-29 17:21:50 +0900

    버그 있는 버전

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const fs = require("fs");
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static("src"));
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/src/html/index.html");
+});
+/*
+socket.io 모듈을 불러와서 io 변수에 담고,
+io.sockets.on에 connection 이벤트가 호출되면 
+실행될 함수를 바인딩해줍니다.
+
+io.sockets은 나를 포함한 모든 소켓의 객체이며 
+'connection' 이벤트는 소켓이 연결되면 
+호출되는 이벤트입니다.
+*/
+io.on("connection", (socket) => {
+  // 연결되고 chat message가 오면
+  filed = JSON.parse(fs.readFileSync("src/base/file.json", "utf-8"));
+  io.emit("load_chat", filed);
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
   });
+  // msg를 보내준다
+  socket.on("save message", function (msg) {
+    filed = JSON.parse(fs.readFileSync("src/base/file.json", "utf-8"));
+    filed.list[filed.list.length] = msg;
+    fs.writeFileSync("./src/base/file.json", filed);
+  });
+  socket.on("disconnect", function () {
+    console.log("탈퇴");
+  });
+  console.log("접속");
 });
 
 http.listen(port, () => {
